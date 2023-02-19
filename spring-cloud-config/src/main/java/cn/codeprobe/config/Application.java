6cdf9ca32cd3a553909a1212a412a782ffb5d398
Lionido778 <codeprobe@163.com>
2023-02-19 18:22:40 +0800

    v1.0.1 搭建 config 配置中心，并读取git远程仓库的配置文件

--- spring-cloud-config/src/main/java/cn/codeprobe/config/Application.java
+++ spring-cloud-config/src/main/java/cn/codeprobe/config/Application.java
@@ -3,10 +3,9 @@ package cn.codeprobe.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -16,9 +15,10 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Lionido
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class,
-    RabbitAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
+    RabbitAutoConfiguration.class})
 @ComponentScan({"cn.codeprobe"})
 @EnableEurekaClient
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
